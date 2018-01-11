d292df7fab2fadb080007109b764aa1246b92d2a
apurvacreator <apurva.mukherjee7@gmail.com>
2018-01-12 00:49:46 +0530

    assignment committed

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component';
+import { AlertsComponent } from './alerts/alerts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlertsComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
